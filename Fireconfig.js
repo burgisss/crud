59749f54c5ea731879e4c9c2965e28f553a8b249
A4Tech <169032460+burgisss@users.noreply.github.com>
2024-06-09 13:08:50 +0800

    Create Fireconfig.js

---
+++ Fireconfig.js
@@ -0,0 +1,21 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdtToBR6whDr5txoVQTcBiYBugbO55PFA",
+  authDomain: "crudkopo.firebaseapp.com",
+  databaseURL: "https://crudkopo-default-rtdb.firebaseio.com",
+  projectId: "crudkopo",
+  storageBucket: "crudkopo.appspot.com",
+  messagingSenderId: "524888536303",
+  appId: "1:524888536303:web:3b61929b1a6008a8c4971a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+  
+}
